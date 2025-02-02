385993e185774383e6faa2452915b1045f16c900
thegausiantheory <mdgausulreza@gmail.com>
2025-02-03 00:51:08 +0100

    stability fixes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
-// @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
+const host = process.env.TAURI_DEV_HOST ?? false;
+
+export default defineConfig({
   plugins: [sveltekit()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
+    host: host,
     hmr: host
       ? {
           protocol: "ws",
@@ -25,8 +23,12 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+  resolve: {
+    alias: {
+      $lib: resolve(__dirname, "./src/lib"),
+    },
+  },
+});
