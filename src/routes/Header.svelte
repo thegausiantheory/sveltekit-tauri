c619d6cf8aef35d55c0b5526ba524bb51737f5dc
thegausiantheory <mdgausulreza@gmail.com>
2025-02-02 23:48:27 +0100

    fixes 2

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,6 +19,9 @@
 			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
+			<li aria-current={page.url.pathname === '/home' ? 'page' : undefined}>
+				<a href="/tauri">Tauri</a>
+			</li>
 			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
 			</li>
